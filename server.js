cd48f6196b4268d02ea3ace4f2a71524d53ee34f
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-01-15 21:11:10 -0700

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var process = require("process");
 
 // Sets up the Express App
 // =============================================================
